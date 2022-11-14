8be2bad4e65e48df9b924f8bb67e5186669c461f
zhaokunsong <zhaokunsong@weiyankeji.cn>
2022-11-14 17:21:40 +0800

    feat: 🚀 添加全局 header 和 footer

--- layouts/BaseLayout.vue
+++ layouts/BaseLayout.vue
@@ -1,84 +1,246 @@
 <template>
-  <div>
-    <div id="index_header">
-      <div class="header" id="header_nav">
-        <div class="header-top">
-          <div class="header-tl">
-            <a href="/index.html"
-              ><img src="https://fecdn.weiyankeji.cn/project/simple/wywebsite/1.16/images/index/logo.png" alt=""
-            /></a>
-            <span>普惠的人工智能基础设施提供商</span>
-          </div>
-          <div class="fr">
-            <a href="index.html">首页</a>
-            <a href="/about.html" class="">关于我们</a>
-            <a href="/team.html" class="">微言团队</a>
-            <!-- <a href="/solution.html" class="">解决方案<i></i></a> -->
-            <a href="/dynamic.html" class="">微言动态</a>
-            <a href="/join.html" class="router-link-active">加入我们</a>
-          </div>
+  <div id="container">
+    <header>
+      <div class="header">
+        <div class="header-left">
+          <NuxtLink to="/">
+            <img src="https://fecdn.weiyankeji.cn/project/simple/wywebsite/1.16/images/index/logo.png" alt="" />
+          </NuxtLink>
+          <span>普惠的人工智能基础设施提供商</span>
+        </div>
+        <div class="header-right">
+          <NuxtLink to="/">首页</NuxtLink>
+          <NuxtLink to="/about">关于我们</NuxtLink>
+          <NuxtLink to="/team">微言团队</NuxtLink>
+          <NuxtLink to="/news">微言动态</NuxtLink>
+          <NuxtLink to="/join">加入我们</NuxtLink>
         </div>
       </div>
-    </div>
+    </header>
 
-    <div>
+    <div class="main">
       <slot />
     </div>
 
-    <div id="index_footer">
+    <footer>
       <div class="footer">
         <div class="footer-detail">
-          <div class="left">
-            About us
-            <div>
-              <h6>关于<i>微言</i></h6>
-              <p>
-                微言科技，成立于2017年，是普惠的人工智能基础设施提供商。公司基于AutoML自动建模平台及隐私计算技术，为政府、金融机构及企业提供PaaS+SaaS服务，助力其加速数字化变革。微言科技已经获得包括软银中国在内的顶级投资机构投资，目前在北京、上海及深圳均设有分支机构。
-              </p>
+          <div class="footer-detail-left">
+            <div class="translate">About us</div>
+            <h6>关于<i>微言</i></h6>
+            <p>
+              微言科技，成立于2017年，是普惠的人工智能基础设施提供商。公司基于AutoML自动建模平台及隐私计算技术，为政府、金融机构及企业提供PaaS+SaaS服务，助力其加速数字化变革。微言科技已经获得包括软银中国在内的顶级投资机构投资，目前在北京、上海及深圳均设有分支机构。
+            </p>
+          </div>
+          <div class="footer-detail-center">
+            <div class="translate">Quick line</div>
+            <h6>快速链接</h6>
+            <div class="router">
+              <NuxtLink to="/about">关于我们</NuxtLink>
+              <NuxtLink to="/team">微言团队</NuxtLink>
+              <NuxtLink to="/news">微言动态</NuxtLink>
+              <NuxtLink to="/join">加入我们</NuxtLink>
             </div>
           </div>
-          <div class="center">
-            Quick line
-            <div>
-              <h6>快速链接</h6>
-              <ul>
-                <li><a href="/about.html">关于我们</a></li>
-                <li><a href="/team.html">微言团队</a></li>
-                <li><a href="/dynamic.html">微言动态</a></li>
-                <li><a href="/join.html">加入我们</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="right">
-            Contact us
-            <div>
-              <h6>联系我们</h6>
-              <div class="contact-info">
-                <div class="weiyan-code">
-                  <img src="https://fecdn.weiyankeji.cn/project/simple/wywebsite/1.16/images/index/code.png" alt="" />
-                </div>
-                <div class="address">
-                  <p><span>深圳总部：</span>深圳市福田区凯丰路10号国际金融科技城13层</p>
-                  <p><span>上海分公司：</span>上海市黄浦区外滩街道四川中路33号创业大楼617室</p>
-                  <p><span>北京分公司：</span>北京市西城区黄寺大街甲23号院北广大厦A口6层634</p>
-                  <!-- <p><span>广州分公司：</span>广州高新技术产业开发区科丰路85号2007房</p> -->
-                  <div><span>T：0755-8279 5265</span><span>E：BD@weiyankeji.cn</span></div>
-                </div>
+          <div class="footer-detail-right">
+            <div class="translate">Contact us</div>
+            <h6>联系我们</h6>
+            <div class="contact-info">
+              <div class="weiyan-code">
+                <img src="https://fecdn.weiyankeji.cn/project/simple/wywebsite/1.16/images/index/code.png" alt="" />
+              </div>
+              <div class="address">
+                <p><span>深圳总部：</span>深圳市福田区凯丰路10号国际金融科技城13层</p>
+                <p><span>上海分公司：</span>上海市黄浦区外滩街道四川中路33号创业大楼617室</p>
+                <p><span>北京分公司：</span>北京市西城区黄寺大街甲23号院北广大厦A口6层634</p>
+                <div><span>T：0755-8279 5265</span><span>E：BD@weiyankeji.cn</span></div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="bottom">
-        <div class="bottom_center">
+        <div class="footer-bottom">
           <div>
-            Copyright&copy;2021 深圳微言科技有限责任公司 版权所有
+            <span>Copyright&copy;2021 深圳微言科技有限责任公司 版权所有 </span>
             <a href="https://beian.miit.gov.cn" target="_blank">粤ICP备17076300号-1</a>
           </div>
         </div>
       </div>
-    </div>
+    </footer>
   </div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style scoped lang="scss">
+header {
+  position: sticky;
+  top: 0;
+  background-color: #ffffff;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    padding: 28px 0;
+    width: 1170px;
+    height: 32px;
+    line-height: 32px;
+    &-left {
+      display: flex;
+      align-items: center;
+      font-size: 16px;
+      a {
+        display: inline-block;
+        width: 132px;
+        min-width: 132px;
+        height: 32px;
+        cursor: pointer;
+        img {
+          width: 100%;
+        }
+      }
+      span {
+        margin: 0 18px;
+        padding-left: 18px;
+        border-left: 2px solid #ededed;
+        color: #5c6566;
+      }
+    }
+    &-right {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      a {
+        position: relative;
+        margin-right: 32px;
+        line-height: 28px;
+        text-decoration: none;
+        font-size: 14px;
+        color: #000000;
+        &:last-child {
+          margin-right: 0;
+        }
+        &:hover {
+          color: #00cdc4;
+        }
+        &.router-link-active {
+          font-weight: 700;
+          color: #00cdc4;
+          &::before {
+            position: absolute;
+            left: -7px;
+            top: 5px;
+            width: 2px;
+            height: 18px;
+            background-color: #00cdc4;
+            content: "";
+          }
+        }
+      }
+    }
+  }
+}
+footer {
+  padding-top: 60px;
+  background-color: #2d3240;
+  color: #919899;
+  .footer {
+    &-detail {
+      display: flex;
+      margin: 0 auto;
+      width: 1170px;
+      height: 270px;
+      font-weight: 300;
+      &-left {
+        position: relative;
+        margin-right: 90px;
+        width: 300px;
+        p {
+          line-height: 26px;
+          font-size: 14px;
+        }
+      }
+      &-center {
+        position: relative;
+        margin-right: 84px;
+        width: 115px;
+        .router {
+          display: flex;
+          flex-direction: column;
+          a {
+            margin-bottom: 24px;
+            line-height: 16px;
+            text-decoration: none;
+            font-size: 14px;
+            color: #919899;
+          }
+        }
+      }
+      &-right {
+        position: relative;
+        .contact-info {
+          display: flex;
+          .weiyan-code {
+            margin-right: 16px;
+            width: 128px;
+            height: 128px;
+            img {
+              width: 100%;
+            }
+          }
+          .address {
+            line-height: 16px;
+            font-size: 14px;
+            p {
+              margin-bottom: 8px;
+            }
+            span {
+              color: #c8cccc;
+            }
+            div {
+              margin-top: 48px;
+              span:first-child {
+                margin-right: 30px;
+              }
+            }
+          }
+        }
+      }
+      .translate {
+        margin-bottom: 30px;
+        line-height: 32px;
+        font-size: 24px;
+        color: rgb(255 255 255 / 10%);
+      }
+      h6 {
+        position: absolute;
+        top: 18px;
+        line-height: 20px;
+        font-weight: 400;
+        font-size: 14px;
+        color: #ffffff;
+        i {
+          font-style: normal;
+          color: #01cada;
+        }
+      }
+    }
+    &-bottom {
+      padding: 24px 0;
+      border-top: 1px solid rgb(255 255 255 / 10%);
+      div {
+        margin: 0 auto;
+        width: 1170px;
+        line-height: 20px;
+        text-align: center;
+        font-size: 14px;
+        color: #c8cccc;
+        a {
+          text-decoration: underline;
+          color: #c8cccc;
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/news.vue
+++ pages/news.vue
@@ -3,8 +3,70 @@
     <div v-for="i in 5" :key="i">
       <NuxtLink :to="`/article/${i}`">jump to news {{ i }}</NuxtLink>
     </div>
+    <div class="test">
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>666</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+      <div>888</div>
+    </div>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts"></script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.test {
+  div {
+    background-color: coral;
+  }
+}
+</style>
